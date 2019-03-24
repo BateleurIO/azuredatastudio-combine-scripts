ebd6e581c05870edc2ebe08f15e0344084979575
Cobus Kruger <cobus@bateleur.io>
2019-03-25 01:17:34 +0200

    Changed it to use the new `@cobuskruger/combine-files` package.

--- src/extension.ts
+++ src/extension.ts
@@ -3,29 +3,42 @@
 import * as vscode from 'vscode';
 // import * as sqlops from 'sqlops';
 import * as fs from 'fs';
-import * as tmp from 'tmp';
-import { FullFileList } from './full-file-list';
-import { FileConcatenator } from './file-concat';
-import { BateleurConfig } from './bateleur-config';
 import * as path from 'path';
+import * as tmp from 'tmp';
+import { ConfigLoader, FileList, FileCombiner } from '@cobuskruger/combine-files';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.combineScripts', async(selectedFile: any, fileList: any) => {
-        const rootPath = <any>vscode.workspace.rootPath;
-        const fileGroups = new BateleurConfig(rootPath).get.fileGroups;
+        const rootPath = <any>vscode.workspace.rootPath || './';
+        const vscodeConfig = vscode.workspace.getConfiguration('combineScripts');
+        const configLoader = new ConfigLoader({ fileGroups: [vscodeConfig as any] });
+        const configFilePath = path.resolve(rootPath, '.combinefilesrc.json');
+        if (fs.existsSync(configFilePath)) {
+            configLoader.addConfigFile(configFilePath);
+        }
+        const fileGroups = configLoader.config.fileGroups;
         const outputFiles = [];
         if (!fileList) {
             fileList = [];
         }
-        if (fileList.length === 0) {
+        if (fileList.length > 0) {
+            if (fileList[0] instanceof vscode.Uri) {
+                fileList = fileList.map(item => item.fsPath);
+            }
+        } else {
             if (selectedFile) {
-                fileList.push(selectedFile);
+                if (selectedFile instanceof vscode.Uri) {
+                    fileList.push(selectedFile.fsPath)
+                } else {
+                    fileList.push(selectedFile);
+                }
             } else {
                 fileList.push(rootPath);
             }
         }
-        for (const config of fileGroups) {
-            const files = (new FullFileList(config, rootPath, fileList)).list;
+        for (const fileGroup of fileGroups) {
+            const resolvedFiles = (new FileList(fileList, rootPath, fileGroup));
+            const files = resolvedFiles.list;
             const fileLists = {};
             for (const file of files) {
                 const ext = <any>(file.split('.').pop());
@@ -34,21 +47,21 @@ export function activate(context: vscode.ExtensionContext) {
                 }
                 fileLists[ext].push(file);
             }
+
             for (const key in fileLists) {
                 if (!fileLists.hasOwnProperty(key)) {
                     continue;
                 }
-                let text = (new FileConcatenator(config, fileLists[key])).getText();
+                const fileText = (new FileCombiner(fileGroup, fileLists[key])).getText();
+                let outputFileName = fileGroup.outputFileName || tmp.tmpNameSync() + '.' + key;
 
-                let outputFileName = config.outputFileName || tmp.tmpNameSync() + '.' + key;
                 if (!path.isAbsolute(outputFileName)) {
                     outputFileName = path.join(rootPath, outputFileName);
                 }
-                fs.writeFileSync(outputFileName, text);
+                fs.writeFileSync(outputFileName, fileText);
                 outputFiles.push(outputFileName);
             }
         }
-
         for (const outputFileName of outputFiles) {
             const doc = await vscode.workspace.openTextDocument(outputFileName);
             await vscode.window.showTextDocument(doc, { preview: false });
